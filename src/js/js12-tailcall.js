d017207255c22a235518f8acfe0e09cc109c4834
chenxing <1066346604@qq.com>
2018-01-18 13:31:44 +0800

    update tailcall.js

--- src/js/js12-tailcall.js
+++ src/js/js12-tailcall.js
@@ -1,7 +1,9 @@
 /* tail call
-   ref:http://www.ruanyifeng.com/blog/2015/04/tail-call.html
+   ref:
+   http://www.ruanyifeng.com/blog/2015/04/tail-call.html
+   http://www.zcfy.cc/article/all-about-recursion-ptc-tco-and-stc-in-javascript-2813.html
 */
-
+//'use strict'
 //1.定义：函数在最后一步操作调用另一函数、并且没有其他运算
 //函数调用会形成调用栈(call stack)
 //f1调用f2，f2压栈f1栈，f2执行完出栈，f1执行完出栈
@@ -41,6 +43,7 @@ console.log(factorial(5,1));
 console.log('------3.tail recursive opti-------');
 //opt 1
 var tailfactorial=function(n,total){
+    //console.trace();
     if(n<=1)return total;
     return tailfactorial(n-1,n*total);
 }
@@ -56,6 +59,7 @@ function currying(fn,n){
     }
 }
 var tailfactorial=function(n,total){
+    //console.trace();
     if(n<=1)return total;
     return tailfactorial(n-1,n*total);
 }
@@ -64,7 +68,21 @@ console.log(factorial(5));
 
 //opt 3:
 var factorial=function(n,total=1){
+    //console.trace();
     if(n<=1)return total;
-    return tailfactorial(n-1,n*total);
+    return factorial(n-1,n*total);
+}
+//console.log(factorial(50000));//RangeError: Maximum call stack size exceeded
+console.log(factorial(6));
+
+//opt 4:
+var factorial=function(n,total=1){
+    while(n>=0){
+    //console.trace();
+    if(n<=1)
+        return total;
+    total=n*total;
+    n=n-1;
+    }
 }
 console.log(factorial(5));
