1c289cf55c1b9819d9f389dc7823628b0655e514
chenxing <1066346604@qq.com>
2018-01-30 17:46:42 +0800

    update  js eventloop

--- src/js/js11-eventloop.js
+++ src/js/js11-eventloop.js
@@ -34,11 +34,12 @@ req.end();
 
 //2.timer 定时器  定时器：setTimeout setInterval 内部机制相同，前者执行一次，后者反复执行
 console.log('-------Timer---------');
-console.log('1');
-setTimeout(function(){ console.log('2'); },0);
-console.log('3');
+console.log('1');//执行栈
+setTimeout(function(){ console.log('2'); },0);//加入事件队列
+console.log('3');//执行栈
 //nextTick 是在 当前执行栈尾部、下次EventLoop读取事件队列前 执行回调函数
 process.nextTick(function(arg){
+    console.log('--------------Next Event Loop-----------------');
     console.log(arg);
 },'nextTick callback');
 //有多个nextTick不管是否嵌套都会在当前执行栈尾部执行
@@ -49,8 +50,8 @@ process.nextTick(function(){
     });
 });
 
-console.log('4');
-
+console.log('4');//执行栈
+//setImmediate 是在 下次EventLoop读取事件队列执行。
 setImmediate(function(arg){
     console.log(arg);
 },'setImmediate callback');
@@ -61,5 +62,18 @@ setImmediate(function(){
     });
 });
 
-console.log('5');
-setTimeout(function(){ console.log('setTimeout callback'); },0);
+console.log('5');//当前执行栈
+setTimeout(function(){ 
+    console.log('--------------Next Event Loop-----------------');
+    console.log('setTimeout callback');
+
+    setTimeout(function(){
+        console.log('setTimeout nested in setTimeout');
+    },0);
+    setImmediate(function(){
+        console.log('setImmediate nested in setTimeout');
+    });
+    process.nextTick(function(){
+        console.log('nextTick nested in setTimeout');
+    });
+},10);//加入事件队列
