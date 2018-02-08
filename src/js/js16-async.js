9066b11809e0b9babb07aadda439cd7a7bf4ce7c
chenxing <1066346604@qq.com>
2018-02-08 17:39:52 +0800

    update js async

--- src/js/js16-async.js
+++ src/js/js16-async.js
@@ -1,6 +1,14 @@
 /*  异步编程系列04
-    Async函数就是Generator函数的语法糖
+    async函数就是generator函数的语法糖
     ref:http://www.ruanyifeng.com/blog/2015/05/async.html
+
+    async优点：
+    1.内置执行器 
+        async能够自动执行，generator函数需要依靠thunk promise 或者co执行
+    2.更好的语义 
+    3.更广的适用性 
+        co函数规定yield后为thunk或promise
+        async await后可以是promise或原始类型-原始类型为同步操作
 */
 
 //1. what is async
@@ -23,3 +31,36 @@ var asyncreadfile=async function(){
 
 asyncreadfile();
 
+//2.async await实现  将generator函数和自动执行器包装在一个函数里
+
+var spawn=function(gen1){
+    return new Promise(function(resolve,reject){
+        var gen=gen1();
+        var step=function (next){
+            try{
+                var result=next();
+            }catch(e){
+                return reject(e);
+            }
+            if(result.done){
+                return resolve(result.value);
+            }
+            Promise.resolve(result.value).then(function(val){
+                step(function(){ return gen.next(val); });
+            },function(e){
+                step(function(){ return gen.throw(e); });
+            });
+        }
+        step(function(){ return gen.next(undefined); });
+    });
+}
+
+
+var gen=function*(){
+    var f1=yield readfile('tmp/b.txt');
+    var f2=yield readfile('tmp/b.txt');
+    console.log(f1.toString());
+    console.log(f2.toString());
+}
+
+spawn(gen);
