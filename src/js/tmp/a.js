ac40521cc146404bfba72083cb2937497b2b7e70
chenxing <1066346604@qq.com>
2018-02-11 16:24:58 +0800

    add js require

---
+++ src/js/tmp/a.js
@@ -0,0 +1,8 @@
+console.log('-------------module 2-------------');
+console.log(module.id);
+console.log(module.exports);
+console.log(module.parent);
+console.log(module.filename);
+console.log(module.loaded);
+console.log(module.children);
+console.log(module.paths);
