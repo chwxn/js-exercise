db17c4811690181fbca98b1502c3c4e69cff21ea
chenxing <1066346604@qq.com>
2018-02-22 17:21:46 +0800

    update js circular depend

--- src/js/js18-circulardepend.js
+++ src/js/js18-circulardepend.js
@@ -1,5 +1,12 @@
 //参见 src/ts/circulardepend
 
-import {foo} from './tmp/b.js';
-console.log(foo);
-setTimeout(()=>console.log(foo),500);
+// import {foo} from './tmp/b.js';
+// console.log(foo);
+// setTimeout(()=>console.log(foo),500);
+
+
+//根据打印结果：b.js中，仅执行a.js的第一行
+var a=require('./tmp/a');
+var b=require('./tmp/b');//可注释测试，此行无输出
+
+console.log('circulardepend a.done=%j b.done=%j',a.done,b.done);
--- src/js/tmp/b.js
+++ src/js/tmp/b.js
@@ -1,2 +1,8 @@
 
 //var a=require('./a.js');
+
+exports.done = false;
+var a = require('./a.js');
+console.log('b.js a.done = %j', a.done);
+exports.done = true;
+console.log('b.js done');
