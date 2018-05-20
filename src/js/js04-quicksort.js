b38364c95ae270f5a2b62f04b6d122569098711d
chwxn <1066346604@qq.com>
2018-05-20 21:28:11 +0800

    update js quicksort

--- src/js/js04-quicksort.js
+++ src/js/js04-quicksort.js
@@ -1,215 +1,215 @@
 /* quicksort 快排
 */
-var cmp_count=0;
-var swap_count=0;
-function swap(arr,a,b){
+var cmp_count = 0;
+var swap_count = 0;
+function swap(arr, a, b) {
     swap_count++;
-    var tmp=arr[a];
-    arr[a]=arr[b];
-    arr[b]=tmp;
+    var tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
 }
-function cmp(a,b){
+function cmp(a, b) {
     cmp_count++;
-    return a-b;
+    return a - b;
 }
-function checkresult(arr){
-    var val=arr[0];
+function checkresult(arr) {
+    var val = arr[0];
     for (let index = 1; index < arr.length; index++) {
-        if(val>arr[index]){
+        if (val > arr[index]) {
             return false;
         }
-        val=arr[index];
+        val = arr[index];
     }
     return true;
 }
 var count = 2000;
 var arr = [];
-for(var i=0; i<count; i++){
+for (var i = 0; i < count; i++) {
     arr.push(Math.floor(Math.random() * count));
 }
 
 //1.
 //--------quicksort 1 recursive----------
-var ruanyf_quicksort=function(arr){
-    if(arr.length<=1){
+var ruanyf_quicksort = function (arr) {
+    if (arr.length <= 1) {
         return arr;
     }
-    var index=Math.floor(arr.length/2);
-    var value= arr[index];//arr.splice(index,1)[0];
-    var left =[];
-    var right =[];
- //swap_count++;
-    for(var i=0;i<arr.length;i++){
-        if(index!=i){
-            var cur=arr[i];
-            if(cmp(cur,value)<0){
+    var index = Math.floor(arr.length / 2);
+    var value = arr[index];//arr.splice(index,1)[0];
+    var left = [];
+    var right = [];
+    //swap_count++;
+    for (var i = 0; i < arr.length; i++) {
+        if (index != i) {
+            var cur = arr[i];
+            if (cmp(cur, value) < 0) {
                 swap_count++;
                 left.push(cur);
-            }else{
+            } else {
                 swap_count++;
                 right.push(cur);
             }
         }
     }
- //swap_count++;
+    //swap_count++;
     return ruanyf_quicksort(left).concat([value]).concat(ruanyf_quicksort(right));
 }
 
 //2.
 //--------quicksort 2 original array sorted----------
-var partition=function(arr,start,end){
-    var pivot=arr[start];//基准
-    while(start<end){
-        while(start<end&& cmp(pivot,arr[end])<=0 ){
+var partition = function (arr, start, end) {
+    var pivot = arr[start];//基准
+    while (start < end) {
+        while (start < end && cmp(pivot, arr[end]) <= 0) {
             end--;
         }
-        if(start!=end){
+        if (start != end) {
             //[arr[start],arr[end]]=[arr[end],arr[start]];
-            swap(arr,start,end);
+            swap(arr, start, end);
         }
-        while(start<end&& cmp(pivot,arr[start])>=0){
+        while (start < end && cmp(pivot, arr[start]) >= 0) {
             start++;
         }
-        if(start!=end){
+        if (start != end) {
             //[arr[start],arr[end]]=[arr[end],arr[start]];
-            swap(arr,start,end);
+            swap(arr, start, end);
         }
     }
     return start;
 }
-var qsort=function(arr,start,end){
-    if(start>=end){
+var qsort = function (arr, start, end) {
+    if (start >= end) {
         return;
     }
-    var mid=partition(arr,start,end);
-    qsort(arr,start,mid-1);
-    qsort(arr,mid+1,end);
+    var mid = partition(arr, start, end);
+    qsort(arr, start, mid - 1);
+    qsort(arr, mid + 1, end);
 }
 
 //3. qsort_hoare 
-function partition_hoare(arr,start,end){
-    var pivot=arr[start];
-    var l=start; var r=end;
-    while(1){
-        while(cmp(arr[l],pivot)<0){
+function partition_hoare(arr, start, end) {
+    var pivot = arr[start];
+    var l = start; var r = end;
+    while (1) {
+        while (cmp(arr[l], pivot) < 0) {
             l++;
         }
-        while(cmp(pivot,arr[r])<0){
+        while (cmp(pivot, arr[r]) < 0) {
             r--;
         }
-        if(l==r){
+        if (l == r) {
             return l;
-        }else if(l>r){
-            return l-1;
+        } else if (l > r) {
+            return l - 1;
         }
-        swap(arr,l,r);
+        swap(arr, l, r);
         l++; r--;
     }
 }
-function qsort_hoare(arr,start,end){
-    if(start>=end){
+function qsort_hoare(arr, start, end) {
+    if (start >= end) {
         return;
     }
-    var p=partition_hoare(arr,start,end);
-    qsort_hoare(arr,start,p);
-    qsort_hoare(arr,p+1,end);
+    var p = partition_hoare(arr, start, end);
+    qsort_hoare(arr, start, p);
+    qsort_hoare(arr, p + 1, end);
 }
 
-function qsort_hoare_improve(arr,start,end){
-    var pivot =arr[start];
-    var l=start+1; var r=end;
-    while(l<=r){
-        while(l<=r&&cmp(arr[l],pivot)<=0){
+function qsort_hoare_improve(arr, start, end) {
+    var pivot = arr[start];
+    var l = start + 1; var r = end;
+    while (l <= r) {
+        while (l <= r && cmp(arr[l], pivot) <= 0) {
             l++;
         }
-        if(l>r){
+        if (l > r) {
             break;
         }
-        while(l<=r&&cmp(pivot,arr[r])<=0){
+        while (l <= r && cmp(pivot, arr[r]) <= 0) {
             r--;
         }
-        if(l>r){
+        if (l > r) {
             break;
         }
-        swap(arr,l,r);
-        l++;r--;
+        swap(arr, l, r);
+        l++; r--;
     }
-    swap(arr,start,r);
-    if(start<r-1){
-        qsort_hoare_improve(arr,start,r-1);
+    swap(arr, start, r);
+    if (start < r - 1) {
+        qsort_hoare_improve(arr, start, r - 1);
     }
-    if(l<end){
-        qsort_hoare_improve(arr,l,end);
+    if (l < end) {
+        qsort_hoare_improve(arr, l, end);
     }
 }
 
 //4. qsort_lomuto
-function partition_lomuto(arr,start,end){
-    var pivot=arr[end];
-    var s=start;
+function partition_lomuto(arr, start, end) {
+    var pivot = arr[end];
+    var s = start;
     for (var p = start; p < end; p++) {
-        if(cmp(arr[p],pivot)<0){
-            if(s!=p){
-                swap(arr,s,p);
+        if (cmp(arr[p], pivot) < 0) {
+            if (s != p) {
+                swap(arr, s, p);
             }
             s++;
         }
     }
-    if(s==end){
-        return s-1;
-    }else{
-        swap(arr,s,end);
+    if (s == end) {
+        return s - 1;
+    } else {
+        swap(arr, s, end);
         return s;
     }
 }
-function qsort_lomuto(arr,start,end){
-    if(start>=end){
+function qsort_lomuto(arr, start, end) {
+    if (start >= end) {
         return;
     }
-    var p=partition_lomuto(arr,start,end);
-    qsort_lomuto(arr,start,p);
-    qsort_lomuto(arr,p+1,end);
+    var p = partition_lomuto(arr, start, end);
+    qsort_lomuto(arr, start, p);
+    qsort_lomuto(arr, p + 1, end);
 }
 
 //. wintercn quicksort
-function wintercn_qsort(arr,start,end){
-    var midVal=arr[start];
-    var l=start+1;
-    var r=end;
-    while(l<r){
-        while(cmp(arr[l],midVal)>0 && l<r){
-            swap(arr,l,r--);
+function wintercn_qsort(arr, start, end) {
+    var midVal = arr[start];
+    var l = start + 1;
+    var r = end;
+    while (l < r) {
+        while (cmp(arr[l], midVal) > 0 && l < r) {
+            swap(arr, l, r--);
         }
         l++;
     }
-    var mid=arr[r]>midVal?r-1:r;
-    swap(arr,start,mid);
-    if(start<mid){
-        wintercn_qsort(arr,start,mid-1);
+    var mid = arr[r] > midVal ? r - 1 : r;
+    swap(arr, start, mid);
+    if (start < mid) {
+        wintercn_qsort(arr, start, mid - 1);
     }
-    if(mid<end){
-        wintercn_qsort(arr,mid+1,end);
+    if (mid < end) {
+        wintercn_qsort(arr, mid + 1, end);
     }
 }
 
-function test(fn){
-    cmp_count=0;
-    swap_count=0;
-    var arr1=arr.slice(0,arr.length-1);
-    var st=(new Date()).getTime();
-    var arr_result = fn(arr1,0,arr1.length-1);
-    var et=(new Date()).getTime();
-    if(fn===ruanyf_quicksort){
-        var pass= checkresult(arr_result);
-    }else{
+function test(fn) {
+    cmp_count = 0;
+    swap_count = 0;
+    var arr1 = arr.slice(0, arr.length - 1);
+    var st = (new Date()).getTime();
+    var arr_result = fn(arr1, 0, arr1.length - 1);
+    var et = (new Date()).getTime();
+    if (fn === ruanyf_quicksort) {
+        var pass = checkresult(arr_result);
+    } else {
         var pass = checkresult(arr1);
     }
-    if(!pass){
-        console.log(''+fn.name+' quicksort fail!');
+    if (!pass) {
+        console.log('' + fn.name + ' quicksort fail!');
         return;
     }
     console.log(fn.name);
-    console.log('    swap_count:'+swap_count+' cmp_count:'+cmp_count+' time:'+ (et-st));
+    console.log('    swap_count:' + swap_count + ' cmp_count:' + cmp_count + ' time:' + (et - st));
 }
 
 test(ruanyf_quicksort);
